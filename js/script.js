0c1fad2299034c93e53a53f0ebe45753fea3bf4e
Damilola <immanueladeniyi7@gmail.com>
2023-02-07 23:31:49 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,18 +20,3 @@ window.addEventListener("load", () => {
         document.body.removeChild(preloader);
     })
 });
-
-
-///slider
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(elems, options);
-
-});
-
-// Or with jQuery
-
-$(document).ready(function () {
-    $('.slider').slider();
-});
-instances.start();
